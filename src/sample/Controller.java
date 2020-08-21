f8b0498351ee256707f4fbe5875b4e64c5ecfd30
arjunmetre <arjunprojectexecute@gmail.com>
2020-08-21 13:48:52 +0800

    updated code for print reciept

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,9 +4,11 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.text.DecimalFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -72,6 +74,10 @@ public class Controller implements Initializable {
 	@FXML
 	private TextField fieldSalmon;
 
+	private StringBuilder builder = new StringBuilder();
+	private String mainAlign = "%-15s \t %-4s \t  %-4s %n";
+	private String subAlign = "  %-15s \t %-4s \t  %-4s %n";
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
@@ -80,43 +86,7 @@ public class Controller implements Initializable {
 		checkBoxEnable(true);
 
 		btnTotal.setOnAction(event -> {
-			double pretaxOrderPrice = 0.0D;
-			double salesTax = 0.0D;
-			double totalOrderPrice = 0.0D;
-
-			int chickenCount = parseFieldInteger(fieldChicken.getText());
-			int salmonCount = parseFieldInteger(fieldSalmon.getText());
-			int regularCount = parseFieldInteger(fieldRegular.getText());
-			int cappaccinoCount = parseFieldInteger(fieldCapp.getText());
-			int cafeLaitCount = parseFieldInteger(fieldCafe.getText());
-
-			pretaxOrderPrice = chickenCount * chickenSandwichPrice + salmonCount * salmonSandwichPrice
-					+ regularCount * regularCoffeePrice + cappaccinoCount * cappuccinoPrice
-					+ cafeLaitCount * cafeAuLaitPrice;
-			if (chBlueberry.isSelected()) {
-				pretaxOrderPrice += blueberryJamPrice;
-			}
-			if (chButter.isSelected()) {
-				pretaxOrderPrice += butterPrice;
-			}
-			if (chCreamCheese.isSelected()) {
-				pretaxOrderPrice += creamCheesePrice;
-			}
-			if (chPeach.isSelected()) {
-				pretaxOrderPrice += peachJellyPrice;
-			}
-			if (chRaspberry.isSelected()) {
-				pretaxOrderPrice += raspberryJamPrice;
-			}
-
-			salesTax = pretaxOrderPrice * 0.13D;
-			totalOrderPrice = pretaxOrderPrice + salesTax;
-
-			System.out.println(totalOrderPrice + " " + pretaxOrderPrice + " " + salesTax);
-
-			lbSubtotal.setText(df.format(pretaxOrderPrice));
-			lbSalesTax.setText(df.format(salesTax));
-			lbTotalSale.setText(df.format(totalOrderPrice));
+			calculateTotal();
 		});
 
 		btnReset.setOnAction(event -> {
@@ -134,6 +104,112 @@ public class Controller implements Initializable {
 		btnExit.setOnAction(event -> {
 			Platform.exit();
 		});
+	}
+
+	private void calculateTotal() {
+
+		double pretaxOrderPrice = 0.0D;
+		double salesTax = 0.0D;
+		double totalOrderPrice = 0.0D;
+		Date date = new Date();
+		builder.setLength(0);
+		builder.append("**********Cafe Billing Application**********").append("\n");
+		builder.append(date.toString()).append("\n");
+
+		builder.append("Item:\t\t\tQty\t  Amount").append("\n");
+		int chickenCount = parseFieldInteger(fieldChicken.getText());
+		int salmonCount = parseFieldInteger(fieldSalmon.getText());
+		int regularCount = parseFieldInteger(fieldRegular.getText());
+		int cappaccinoCount = parseFieldInteger(fieldCapp.getText());
+		int cafeLaitCount = parseFieldInteger(fieldCafe.getText());
+
+		pretaxOrderPrice = chickenCount * chickenSandwichPrice + salmonCount * salmonSandwichPrice
+				+ regularCount * regularCoffeePrice + cappaccinoCount * cappuccinoPrice
+				+ cafeLaitCount * cafeAuLaitPrice;
+		if (rdChickenSandwich.isSelected()) {
+			builder.append(String.format(mainAlign, "Chicken Sandwich", chickenCount,
+					df.format(chickenCount * chickenSandwichPrice)));
+		}
+		if (rdSalmonSandwich.isSelected()) {
+			builder.append(String.format(mainAlign, "Salmon Sandwich", salmonCount,
+					df.format(salmonCount * salmonSandwichPrice)));
+		}
+		builder.append("Toppings:").append("\n");
+		if (chCreamCheese.isSelected()) {
+			pretaxOrderPrice += creamCheesePrice;
+			builder.append(String.format(subAlign, "Cream Cheese", 1, df.format(creamCheesePrice)));
+		}
+		if (chButter.isSelected()) {
+			pretaxOrderPrice += butterPrice;
+			builder.append(String.format(subAlign, "Butter", 1, df.format(creamCheesePrice)));
+		}
+
+		if (chBlueberry.isSelected()) {
+			pretaxOrderPrice += blueberryJamPrice;
+			builder.append(String.format(subAlign, "Blueberry Jam", 1, df.format(blueberryJamPrice)));
+		}
+		if (chRaspberry.isSelected()) {
+			pretaxOrderPrice += raspberryJamPrice;
+			builder.append(String.format(subAlign, "Raspberry Jam", 1, df.format(creamCheesePrice)));
+		}
+
+		if (chPeach.isSelected()) {
+			pretaxOrderPrice += peachJellyPrice;
+			builder.append(String.format(subAlign, "Peach Jelly", 1, df.format(creamCheesePrice)));
+		}
+
+		if (rdRegular.isSelected()) {
+			builder.append(String.format(mainAlign, "Regular Coffee", regularCount,
+					df.format(regularCount * regularCoffeePrice)));
+		}
+		if (rdCappaccino.isSelected()) {
+			builder.append(String.format(mainAlign, "Cappuccino", cappaccinoCount,
+					df.format(cappaccinoCount * cappuccinoPrice)));
+		}
+		if (rdCafeAuLait.isSelected()) {
+			builder.append(String.format(mainAlign, "Cafe Au Lait", cafeLaitCount,
+					df.format(cafeLaitCount * cafeAuLaitPrice)));
+		}
+
+		builder.append("\t\t\t-----------------\n");
+		salesTax = pretaxOrderPrice * 0.13D;
+		totalOrderPrice = pretaxOrderPrice + salesTax;
+		builder.append(String.format(mainAlign, "Pretax Total", "", df.format(pretaxOrderPrice)));
+		builder.append(String.format(mainAlign, "Sales Tax 13%", "", df.format(salesTax)));
+		builder.append(String.format(mainAlign, "Total Sale", "", df.format(totalOrderPrice)));
+		System.out.println(totalOrderPrice + " " + pretaxOrderPrice + " " + salesTax);
+
+		lbSubtotal.setText(df.format(pretaxOrderPrice));
+		lbSalesTax.setText(df.format(salesTax));
+		lbTotalSale.setText(df.format(totalOrderPrice));
+		builder.append("*********THANK YOU FOR YOUR ORDER*********");
+
+	}
+
+	@FXML
+	private void closeByMenu() {
+		Platform.exit();
+	}
+
+	@FXML
+	private void printReceipt() {
+
+		System.out.println("print Reciept");
+		calculateTotal();
+		System.out.println(builder.toString());
+		Dialog<String> dialog = new Dialog<>();
+		dialog.setTitle("Receipt");
+
+		DialogPane dialogPane = dialog.getDialogPane();
+		dialogPane.getButtonTypes().addAll(ButtonType.CLOSE);
+		TextArea area = new TextArea();
+		area.setText(builder.toString());
+		area.setPrefHeight(300);
+		area.setPrefWidth(500);
+		dialogPane.setContent(new VBox(8, area));
+		dialogPane.getScene().getWindow().sizeToScene();
+		dialog.setResizable(true);
+		dialog.show();
 	}
 
 	private void checkBoxEnable(boolean b) {
